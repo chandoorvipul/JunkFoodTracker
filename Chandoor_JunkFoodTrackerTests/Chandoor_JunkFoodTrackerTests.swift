b9b6c7dc4c6c3e359d6bd3f6ae0462e92d51d166
student <student@vaishnavis-MacBook-Air.local>
2018-03-07 21:44:41 -0600

    Initial Commit

---
+++ Chandoor_JunkFoodTrackerTests/Chandoor_JunkFoodTrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Chandoor_JunkFoodTrackerTests.swift
+//  Chandoor_JunkFoodTrackerTests
+//
+//  Created by student on 3/7/18.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import XCTest
+@testable import Chandoor_JunkFoodTracker
+
+class Chandoor_JunkFoodTrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
